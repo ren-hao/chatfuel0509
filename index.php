f5853a09401ab0a0bc09e14c4ae32d184627854a
NB\suao9 <60211tom@gmail.com>
2018-05-09 23:35:23 +0800

    add method

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'GET'){
 	$text = $_GET['msg'];
 	$message = {
 			 "messages": [
@@ -7,4 +11,5 @@
 			 ]
 			}
 	echo $message;
+}
 >
