b9a81d773cc12994fdbf648fded4ef8e6a614357
NB\suao9 <60211tom@gmail.com>
2018-05-10 00:03:57 +0800

    test4

--- index.php
+++ index.php
@@ -4,12 +4,22 @@ $method = $_SERVER['REQUEST_METHOD'];
 if (isset($_GET['msg']) && ! empty($_GET['msg'])){
 	$text = $_GET['msg'];
 	$message = array(
-				"message" => array(
-					"text" => "Welcome to our store!"
-				)
+				"message" => [array(
+					"text" => "Welcome to our store!",
+					"text" => $text
+				)]
 			);
 	echo json_encode($message);
 }
+/*
+{
+ "messages": [
+   {"text": "Welcome to our store!"},
+   {"text": "How can I help you?"}
+ ]
+}
+*/
+
 /* Process only when method is POST
 if($method == 'GET'){
 	$text = $_GET['msg'];
