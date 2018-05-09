927100f44230230ed85d71cc9530881dc60f7954
NB\suao9 <60211tom@gmail.com>
2018-05-09 23:51:59 +0800

    test2

--- index.php
+++ index.php
@@ -3,7 +3,13 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if (isset($_GET['msg']) && ! empty($_GET['msg'])){
 	$text = $_GET['msg'];
-	echo $text;
+	$message = array(
+				"message" => array(
+			   "text" => "Welcome to our store!",
+			   "text" => "How can I help you?"
+				)
+			)
+	echo json_encode($message);
 }
 /* Process only when method is POST
 if($method == 'GET'){
